b2fada5a75aceb8bb693f1a00522a530f5b54b59
gabriel gama <bielmgama@gmail.com>
2025-06-08 20:13:08 -0300

    feat(frontend): cria componente de busca

---
+++ frontend/src/components/Busca.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
+import apiClient from "../utils/openweathermapClient"
+
+const Busca = ({ onBuscaRealizada, onLoading, resultados }) => {
+	const [termoDeBusca, setTermoDeBusca] = useState("São Paulo")
+
+	useEffect(() => {
+		const buscar = async () => {
+			onLoading(true)
+			try {
+				const response = await apiClient.get("/search", {
+					params: {
+						query: termoDeBusca
+					}
+				})
+				onBuscaRealizada(response.data.list)
+			} catch (error) {
+				console.log(error)
+				onBuscaRealizada(error.response.data)
+			} finally {
+				onLoading(false)
+			}
+		}
+
+		if (termoDeBusca && !resultados) {
+			buscar()
+		}	else {
+			onLoading(true)
+			const timeoutID = setTimeout(() => {
+				if(termoDeBusca.length >= 3) {
+					buscar()
+				}
+			}, 2000)
+
+			return () => {
+				clearTimeout(timeoutID)
+				onLoading(false)
+			}
+		}
+	}, [termoDeBusca])
+
+  return (
+		<div className='w-12 flex flex-column align-items-center justify-content-center'>
+			
+			<div className='w-12 flex justify-content-center'>
+				<IconField iconPosition='left' className=' flex justify-content-center'>
+					<InputIcon className='pi pi-search'/>
+					<InputText 
+						className=''
+						placeholder='Buscar...'
+						onChange={(event) => setTermoDeBusca(event.target.value)}
+						value={termoDeBusca}/>
+				</IconField>
+			</div>
+		</div>
+  )
+}
+
+export default Busca
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import 'primeflex/primeflex.min.css'
+import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/saga-orange/theme.css'
 
 createRoot(document.getElementById('root')).render(
   <App />
